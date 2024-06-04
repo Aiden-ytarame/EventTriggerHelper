7f277070c2250765349d84749b851518b3a638d6
Aiden-ytarame <116085315+Aiden-ytarame@users.noreply.github.com>
2024-06-04 19:07:26 -0300

    fixed stuff

--- EventTriggerHelper/Patchers/SaveDataPatch.cs
+++ EventTriggerHelper/Patchers/SaveDataPatch.cs
@@ -9,42 +9,52 @@ namespace TriggerAPI.Patchers
     [HarmonyPatch(typeof(DataManager))]
     internal class SaveEventType
     {
+        static bool IsSaving = false;
         [HarmonyPatch(nameof(DataManager.SaveData))]
         [HarmonyPrefix]
         //this saves the event type on a EventData, done so multiple mods don't overlap the enum ids of each other
         public static void PreSaveData()
         {
+            IsSaving = true;
             foreach (var dataEntry in DataManager.inst.gameData.beatmapData.eventTriggers.triggers)
             {
                 if ((int)dataEntry.EventType >= Plugin.Inst.DefaultEventsCount)
                 {
-                    dataEntry.EventData.Insert(0, $"(CustomEvent){Il2CppType.Of<EventType>().GetEnumNames()[(int)dataEntry.EventType]}");
+                    dataEntry.EventData.Insert(0,
+                        $"(CustomEvent){Il2CppType.Of<EventType>().GetEnumNames()[(int)dataEntry.EventType]}");
                 }
+
                 if ((int)dataEntry.EventTrigger >= Plugin.Inst.DefaultTriggersCount)
                 {
-                    dataEntry.EventData.Insert(0, $"(CustomTrigger){Il2CppType.Of<TriggerType>().GetEnumNames()[(int)dataEntry.EventTrigger]}");
+                    dataEntry.EventData.Insert(0,
+                        $"(CustomTrigger){Il2CppType.Of<TriggerType>().GetEnumNames()[(int)dataEntry.EventTrigger]}");
                 }
             }
         }
-}
-    [HarmonyPatch(typeof(DataManager._SaveDataNew_d__113))]
-    internal class SaveEventTypeCoroutine
-    {
-        [HarmonyPatch(nameof(DataManager._SaveDataNew_d__113.MoveNext))]
-        [HarmonyPostfix]
-        //removes the event type from event data, so it doesn't show up on the level editor
-        public static void PostSaveData()
+
+        [HarmonyPatch(typeof(VGFunctions.LSFile))]
+        internal class SaveEventTypeCoroutine
         {
-            foreach (var dataEntry in  DataManager.inst.gameData.beatmapData.eventTriggers.triggers)
+            [HarmonyPatch(nameof(VGFunctions.LSFile.WriteToFile))]
+            [HarmonyPostfix]
+            //removes the event type from event data, so it doesn't show up on the level editor
+            public static void PostSaveData()
             {
-                if ((int)dataEntry.EventTrigger >= Plugin.Inst.DefaultTriggersCount)
+                if (!IsSaving) return;
+
+                IsSaving = false;
+                
+                foreach (var dataEntry in DataManager.inst.gameData.beatmapData.eventTriggers.triggers)
                 {
-                    dataEntry.EventData.RemoveAt(0);
-                }
-   
-                if ((int)dataEntry.EventType >= Plugin.Inst.DefaultEventsCount)
-                {
-                    dataEntry.EventData.RemoveAt(0);
+                    if ((int)dataEntry.EventTrigger >= Plugin.Inst.DefaultTriggersCount)
+                    {
+                        dataEntry.EventData.RemoveAt(0);
+                    }
+
+                    if ((int)dataEntry.EventType >= Plugin.Inst.DefaultEventsCount)
+                    {
+                        dataEntry.EventData.RemoveAt(0);
+                    }
                 }
             }
         }
--- Examples/CloseCallTrigger/CloseCallTrigger.cs
+++ Examples/CloseCallTrigger/CloseCallTrigger.cs
@@ -1,5 +1,6 @@
 using System;
 using HarmonyLib;
+using MovingTrigger;
 using UnityEngine;
 using static DataManager.GameData.BeatmapData.EventTriggers;
 
@@ -14,16 +15,15 @@ public class GameManager_Patch
     [HarmonyPostfix]
     static void PostSetup(ref GameManager __instance)
     {
-      
         TriggerType CloseCallType = (TriggerType)TriggerAPI.RegisterTriggerEvents.GetTriggerEnumFromName("Close_Call");
-
+        
         VGPlayerManager.Inst.players.ForEach((System.Action<VGPlayerManager.VGPlayerData>)(player =>
         {
             DataManager.inst.gameData.beatmapData.eventTriggers.triggers.ForEach( (Action<Trigger>)(trigger =>
             {
                 if (trigger.EventTrigger == CloseCallType)
                 {
-                    player.PlayerObject.add_CloseCallEvent(new Action<Vector3>(_pos => { GameManager.Inst.CallEvent(trigger); }));
+                    player.PlayerObject.add_CloseCallEvent(new Action<Vector3>(_pos => { GameManager.Inst.CallEvent(trigger);}));
                 }
             }));
         }));
